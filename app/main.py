22905e53d1f9e9a101b8951611118464c7316ec8
Sebrin <sebrinmudesir@gmail.com>
2024-12-09 14:40:27 +0300

    created the dashboard page and added a side bar, radio button,file upload and ability to choose which columns will be added on the plot

---
+++ app/main.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import matplotlib as plt
+import plotly.express as px
+
+def stats(dataframe):
+    st.header('Data Statistics')
+    st.markdown('#These are some details about your data')
+    st.write(dataframe.describe())
+
+def data_header(dataframe):
+    st.header('Data Header')
+    st.markdown('#These are some examples of your data')
+    st.write(dataframe.head())
+
+def interactive_plot(dataframe):
+    x_axis_val=st.selectbox('Select X-Axis Value', options=dataframe.columns)
+    y_axis_val=st.selectbox('Select Y-Axis Value', options=dataframe.columns)
+
+
+# df=pd.DataFrame()
+st.title("Dashboard Page")
+st.markdown('#This is a page created to design and develop a dashboard using streamlit')
+
+#to add an uploaded file 
+st.sidebar.title('Navigation')
+uploaded_file=st.sidebar.file_uploader("Please Upload Your File Here")
+
+#create a radio button on the navigation
+options = st.sidebar.radio('Pages', options=[
+    'Home','Data Statistics','Data Header','Plot','Interactive Plot'])
+
+#this will take the uploaded file and then find the mean,median and other charachteristcs
+if uploaded_file:
+    
+    df = pd.read_csv(uploaded_file)
+    
+
+    if options =='Data Statistics':
+        stats(df)
+    elif options =='Data Header':
+        data_header(df)
+    elif options =='Interactive Plot':
+        interactive_plot(df)
+
+
+
+    
+    
+    
